5c3bf379a8064f8e8f6ad12d4f2c57813f2d081a
jmmr2asir <jesusmonge2asir@institutodh.net>
2021-10-19 21:35:08 +0200

    segundocommit

--- holamundo.php
+++ holamundo.php
@@ -1,6 +1,8 @@
+<1DOCTYPE htlm>
 <html>
  <head>
   <title>Prueba de PHP</title>
+  <link rel="stylesheet" type="text/css" href="styles.css" />
  </head>
  <body>
  <?php echo '<p>Hola Mundo</p>'; ?>
